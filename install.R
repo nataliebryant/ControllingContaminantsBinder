74b1d36c866aff53a06bc7e9a1f59dbd28e2744a
Lisa Karstens <karstens@ohsu.edu>
2019-06-11 19:31:19 -0700

    first commit

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages(c("knitr", "ggplot2",
+                   "tidyverse", "RColorBrewer", "reshape2", "ggpubr", "caTools", "rprojroot", "dplyr", "knitr", "kableExtra", "gridExtra"))
+source('http://bioconductor.org/biocLite.R')
+biocLite('phyloseq')
+biocLite("decontam")
